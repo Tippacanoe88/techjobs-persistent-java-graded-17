be2ab60c3257dec0ec089913c5239ee8b33ee6fd
Tippacanoe88 <137940796+Tippacanoe88@users.noreply.github.com>
2024-02-21 15:03:22 -0600

    Update 2/21/24

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,40 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "Location is required")
+    @Size(max = 100, message = "Location must be less than or equal to 100 characters")
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id", referencedColumnName = "id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+        // Required by Hibernate
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
